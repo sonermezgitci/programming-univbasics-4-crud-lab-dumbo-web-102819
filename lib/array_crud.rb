d66302ca7cbceeedca6ab9f6b57f5a3f1d35ae35
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-09 15:04:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-retrieve_element_from_index [0]
-retrieve_element_from_index [1]
+retrieve_element_from_index [0][1]
 end
 
 def retrieve_first_element_from_array(array)
