cf6de7c30cf567bc29ff733a1afe5dbf5fbcb21c
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-09 14:36:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,11 +20,10 @@ end
 
 def remove_element_from_start_of_array(array)
   element = array.shift
-
 end
 
 def retrieve_element_from_index(array, index_number)
-
+index[0]
 end
 
 def retrieve_first_element_from_array(array)
